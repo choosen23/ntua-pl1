0afa4dc82defe0d7c7b75035da975547063205c7
choosen23 <filippou.michail@gmail.com>
2019-08-17 04:19:36 +0300

    Agora pl kai python Ztalloc

--- colors/colors.pl
+++ colors/colors.pl
@@ -1,13 +1,3 @@
-/*
- * A predicate that reads the input from File and returns it in
- * the last three arguments: N, K and C.
- * Example:
- *
- * ?- read_input('c1.txt', N, K, C).
- * N = 10,
- * K = 3,
- * C = [1, 3, 1, 3, 1, 3, 3, 2, 2|...].
- */
 read_input(File, N, K, C) :-
     open(File, read, Stream),
     read_line(Stream, [N, K]),
@@ -19,11 +9,36 @@ read_line(Stream, L) :-
     atomic_list_concat(Atoms, ' ', Atom),
     maplist(atom_number, Atoms, L).
 
+contains_all(K, N, _) :-
+    K is N+1.
+contains_all(K, N, List) :-
+    member(K, List),
+    NewK is K+1,
+    contains_all(NewK, N, List).
 
-%occurrences_of_var(2,C,X).
-%thelw na ftiaksw mia lista pou tha kanei occur_var kai 
-%tha exei se kathe thesi poses fores emfanistike h kathe metavliti
+subsequence(L1, L2) :-
+    length(L1, _),
+    subsequence1(L1, L2).
 
+subsequence1([], _).
+subsequence1([X|T1], [X|T2]) :-
+    subsequence2(T1, T2).
+subsequence1(L1, [_|L2]) :-
+    subsequence(L1, L2).
 
-dosomething([]).
-dosomething([H|T]) :- process(H), dosomething(T).
+subsequence2([], _).
+subsequence2([X|T1], [X|T2]) :-
+    subsequence2(T1, T2).
+
+solve(K, List, Answer) :-
+    \+ contains_all(1, K, List),
+    Answer is 0, !.
+
+solve(K, List, Answer) :-
+    subsequence(Ans, List),
+    length(Ans, Answer),
+    contains_all(1, K, Ans),!.
+
+colors(Fname, Answer) :-
+    read_input(Fname, _, K, List),
+    solve(K, List, Answer).
